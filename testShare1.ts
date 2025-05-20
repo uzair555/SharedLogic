f6d64a51ba36c2c6a3c221e8735429e53e6e7e8c
uzair555 <koraiu6@gmail.com>
2025-05-20 19:02:38 +0500

    Updated shared logic code

--- testShare1.ts
+++ testShare1.ts
@@ -1,3 +1,7 @@
 console.log('====================================');
 console.log("Logicccc 1 Shareedddd");
 console.log('====================================');
+
+console.log('====================================');
+console.log("This is my testing which will add in every where");
+console.log('====================================');
